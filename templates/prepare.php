a43d8b7452220a1f925aadcc76151c4282559f2f
David Larrimore <davidlarrimore@gmail.com>
2016-09-01 22:46:03 -0400

    added _parent

--- templates/prepare.php
+++ templates/prepare.php
@@ -9,6 +9,7 @@ $campaignStartDateCountdown = date('Y/m/d', $dateIn);
 ?>
 <html lang="en">
 <head>
+    <base target="_parent" />
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
